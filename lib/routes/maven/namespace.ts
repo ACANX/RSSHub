e5737235582a0ac986fde743df221314ca59a85f
ACANX <245818784@qq.com>
2025-05-27 21:49:13 +0800

    update

--- lib/routes/maven/namespace.ts
+++ lib/routes/maven/namespace.ts
@@ -1,7 +1,7 @@
 import type { Namespace } from '@/types';
 
 export const namespace: Namespace = {
-    name: 'Maven',
+    name: 'maven',
     url: 'https://central.sonatype.com/',
     categories: ['programming'],
     lang: 'zh-CN',
