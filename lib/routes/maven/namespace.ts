101f767a9f76e820ec631615a495f10122331452
ACANX <245818784@qq.com>
2025-05-27 19:43:07 +0800

    add files

---
+++ lib/routes/maven/namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Maven',
+    url: 'https://central.sonatype.com/',
+    categories: ['programming'],
+    lang: 'zh-CN',
+};
